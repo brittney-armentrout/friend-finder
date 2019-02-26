e7441b2edafc1c2e66406df42a6f1a11a1d473bb
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-02-26 10:34:35 -0700

    add questions, update data array, add submit button

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,28 @@
 var friendArray = [
     {
         name: "Milkshakes",
-        photo: "",
-        scores: [5, 3, 5, 4, 4, 2, 1, 1, 1, 4]
+        photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/16730267_963015323734946_7664102975374495842_n.jpg?_nc_cat=104&_nc_ht=scontent.fapa1-2.fna&oh=4bf06fcfa8f5cc0ef48688c64d772930&oe=5D249C13",
+        scores: [3, 5, 1, 2, 3, 3, 5, 3, 5, 5]
     },
     {
         name: "Maple",
-        photo: "",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/51363768_10100582129076276_1697742834863439872_n.jpg?_nc_cat=110&_nc_ht=scontent.fapa1-1.fna&oh=a2e2e522815c25f6f07c5406cd0a967a&oe=5CE8BC8B",
+        scores: [5, 5, 1, 5, 2, 3, 5, 3, 2, 5]
     },
     {
         name: "Karly",
-        photo: "",
-        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/49864473_10158292362348747_1910538368228261888_n.jpg?_nc_cat=111&_nc_ht=scontent.fapa1-1.fna&oh=fb6322724e1c0be37f5c4ea6097c662a&oe=5CDA4893",
+        scores: [3, 4, 3, 5, 3, 2, 4, 4, 3, 2]
     },
     {
         name: "Sean",
-        photo: "",
-        scores: [2, 4, 5, 2, 3, 4, 1, 1, 5, 3]
+        photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/29177134_10100486002010686_3663338166261702164_n.jpg?_nc_cat=105&_nc_ht=scontent.fapa1-2.fna&oh=1fbf24dd6e81f1717634d1823829eab6&oe=5D1F90AF",
+        scores: [5, 5, 1, 3, 3, 3, 3, 3, 1, 5]
+    },
+    {
+        name: "Brittney",
+        photo: "https://www.facebook.com/photo.php?fbid=10105027210476893&l=db9867111f",
+        scores: [5, 5, 1, 5, 5, 4, 2, 4, 4, 5]
     }
 ];
 
